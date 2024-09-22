3efbb3608ee825625ed96b6e4cb01663d7b45070
rushomrong <rushomarak@gmail.com>
2024-09-22 17:21:48 +0600

    git page rediration issue updated

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ document.getElementById("loginBtn").addEventListener("click", function (event) {
   console.log("email and password received");
 
   if (email === "admin@moneycare.com" && password === "1234") {
-    let url = "/home.html";
+    let url = "/mfs-new/home.html";
     url = url.substring(0, url.lastIndexOf("."));
     window.location.href = url;
   } else {
